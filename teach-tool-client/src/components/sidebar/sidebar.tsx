0e99249157f92b79a62c9b8f0f02b9e44d0f7320
Didukh18 <vadimdidukh2000@gmail.com>
2023-12-09 15:55:33 +0200

    Added Link on Menu.Item

--- teach-tool-client/src/components/sidebar/sidebar.tsx
+++ teach-tool-client/src/components/sidebar/sidebar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react';
-
+import Link from 'next/link';
 import {
   SettingFilled,
   TeamOutlined,
@@ -18,45 +18,64 @@ const items = [
     key: '1',
     icon: React.createElement(SettingFilled),
     label: 'Profile',
-    smth: 'fdslg'
+    link: '/profile',
   },
   {
     key: '2',
     icon: React.createElement(ScheduleFilled),
     label: 'Schedule',
+    link: '/schedule',
   },
   {
     key: '3',
     icon: React.createElement(UserOutlined),
     label: 'Pupils',
+    link: '/pupils',
   },
   {
     key: '4',
     icon: React.createElement(TeamOutlined),
     label: 'Groups',
+    link: '/groups',
   },
   {
     key: '5',
     icon: React.createElement(MessageOutlined),
     label: 'Messages',
-  }
+    link: '/messages',
+  },
 ];
 
 const Sidebar: React.FC = () => {
   return (
     <Sider
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          bottom: 0,
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
+    >
+      <div className="demo-logo-vertical" />
+      <Menu
+        onClick={(item) => {
+          console.log(item);
         }}
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['1']}
       >
-        <div className="demo-logo-vertical" />
-        <Menu onClick={(item) => { console.log(item) }} theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} />
-      </Sider>
+        {items.map((item) => (
+          <Menu.Item key={item.key} icon={item.icon}>
+            <Link href={item.link}>
+              <div>{item.label}</div>
+            </Link>
+          </Menu.Item>
+        ))}
+      </Menu>
+    </Sider>
   );
 };
 
